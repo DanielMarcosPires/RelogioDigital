2aefefd78e17effccff2c75bf840ede2be88efde
DanielMarcosPires <89848369+DanielMarcosPires@users.noreply.github.com>
2022-01-06 20:27:55 -0300

    Update clock.js

--- Relogio/clock.js
+++ Relogio/clock.js
@@ -1,25 +1,64 @@
 
-    var estilo = document.querySelector("#tempo")
-    estilo.addEventListener("mouseenter",function(e){
+    var estilo = document.querySelector("#tempo,#autor")
+    var estilo2 = document.querySelector("#autor")
+    estilo2.addEventListener("mouseenter",(e)=>{
+        e.preventDefault()
+        var anim2 
+        anim2 = document.body.querySelector("#autor")
+        anim2.style.background = "#fffc00"
+        anim2.style.color = 'black'
+        anim2.style.cursor = 'pointer'
+    })
+     estilo2.addEventListener("mouseout",(e)=>{
+        e.preventDefault()
+        var anim2 
+        anim2 = document.body.querySelector("#autor")
+        anim2.style.background = null
+        anim2.style.color =null
+    })
+    estilo2.addEventListener("click",(e)=>{
+      
+       e = (n)=>{
+           return n
+       }
+       e = document.querySelector("#autor").innerHTML =e("Daniel Marcos Pires")
+      
+    })
+    estilo2.addEventListener("mouseout",(e)=>{
+      
+        e = (n)=>{
+            return n
+        }
+        e = document.querySelector("#autor").innerHTML =e('Criador:')
+       
+     })
+    estilo.addEventListener("mouseenter",(e)=>{
         
         e.preventDefault()//previnir que apareça default no console do navegador
 
         var anim 
         anim = document.body.querySelector("#tempo")
-        anim.style.background = "blue"
+        anim.style.background = "cyan"
+        anim.style.color = 'black'
             
     })
-   estilo.addEventListener("mouseout",function(e){
+   estilo.addEventListener("mouseout",(e)=>{
        
         e.preventDefault()
 
     var anim
        anim = document.body.querySelector("#tempo")
        anim.style.background = null
+       anim.style.color = null
        
    })
-   
-function showTime(){
+   estilo.addEventListener("click",(e)=>{
+   var e = querySelector(".autor")
+  var texto = e.innerHTML
+     
+    
+   })
+ showTime=()=>{
     var date = new Date();
     var h = date.getHours(); 
     var m = date.getMinutes();
@@ -45,3 +84,4 @@ function showTime(){
 setTimeout(showTime, 1000)
 }
 showTime();
+
